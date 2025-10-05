6ec3376339e5e998b0a573c49d6a85793ce2f086
SDrastal <sa122032@ucf.edu>
2025-10-05 18:50:41 -0400

    Second array for c++

--- TNP Lab 7/Assets/TestSortingTime.cs
+++ TNP Lab 7/Assets/TestSortingTime.cs
@@ -4,13 +4,16 @@ using System.Runtime.InteropServices;
 public class TestSortingTime : MonoBehaviour
 {
     [DllImport("DLLSort", EntryPoint = "TestSort")]
-    public static extern void TestSort(int [] a, int length);
+    public static extern void TestSort(int[] a, int length);
+
     public int[] a;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int[] b;
+
     void Start()
     {
         // Create an array of 1,000,000 unique integers in the range [1,000,000, 1,000,000,000]
         a = new int[1000000];
+        b = new int[1000000];
         System.Random rand = new System.Random();
         System.Collections.Generic.HashSet<int> uniqueNumbers = new System.Collections.Generic.HashSet<int>();
         while (uniqueNumbers.Count < a.Length)
@@ -19,6 +22,8 @@ public class TestSortingTime : MonoBehaviour
             uniqueNumbers.Add(num);
         }
         uniqueNumbers.CopyTo(a);
+        uniqueNumbers.CopyTo(b);
+
         MeasureNativePlugin();
         MeasureManagedCode();
     }
@@ -26,7 +31,7 @@ public class TestSortingTime : MonoBehaviour
     void MeasureNativePlugin()
     {
         var stopwatch = System.Diagnostics.Stopwatch.StartNew();
-        TestSort(a, a.Length);
+        TestSort(b, b.Length);
         stopwatch.Stop();
         Debug.Log($"Native plugin sorting took: {stopwatch.ElapsedMilliseconds} ms");
     }
